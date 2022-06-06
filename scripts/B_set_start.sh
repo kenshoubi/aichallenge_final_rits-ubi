a857e73d622a0f5f148e02eed011d50a4bc61692
kaichi.oda@tier4.jp <kaichi.oda@tier4.jp>
2022-06-06 17:28:52 +0900

    add script

---
+++ scripts/B_set_start.sh
@@ -0,0 +1,3 @@
+
+ros2 topic pub -1 /initialpose geometry_msgs/msg/PoseWithCovarianceStamped\
+ '{header: {frame_id: "map"}, pose: {pose: {position: {x: 3777.7529296875, y: 73700.5078125, z: 1.9112281799316406}, orientation: {x: 0.010548099500745478, y: 0.009977751964807232, z: 0.9728096990302986, w: -0.23115030407711554}}}}'
